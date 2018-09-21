e3f87b01e2160c575ad7dedb319376c3f75d00f7
followin <dlike.version10@gmail.com>
2018-09-22 00:15:15 +0300

    Text has been changed

--- Folls.UI/fe/src/views/Home.vue
+++ Folls.UI/fe/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your news Vue.js App"/>
+    <HelloWorld msg="Welcome to Your news Vue.js App. Some text change"/>
   </div>
 </template>
 
